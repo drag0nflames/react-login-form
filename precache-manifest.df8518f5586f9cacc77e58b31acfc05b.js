9f0fd79dadeced370b3f7f3de863c6b52f312031
Bikky Bhujel <bicky.bhujel@gmail.com>
2019-05-08 20:11:02 +0545

    Updates

---
+++ precache-manifest.df8518f5586f9cacc77e58b31acfc05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9b50708771b7bc20ec4477f5a50da2",
+    "url": "/react-login-form/index.html"
+  },
+  {
+    "revision": "4418b65915860a7cbc4f",
+    "url": "/react-login-form/static/css/main.71234786.chunk.css"
+  },
+  {
+    "revision": "2ff0daa73cde99797ac9",
+    "url": "/react-login-form/static/js/2.7aab553e.chunk.js"
+  },
+  {
+    "revision": "4418b65915860a7cbc4f",
+    "url": "/react-login-form/static/js/main.40b9d5fc.chunk.js"
+  },
+  {
+    "revision": "575c27b04e929487aecf",
+    "url": "/react-login-form/static/js/runtime~main.bf522c06.js"
+  },
+  {
+    "revision": "00b7ec5fde5eea3e98a930eff306ceaf",
+    "url": "/react-login-form/static/media/tl.00b7ec5f.png"
+  }
+]);
